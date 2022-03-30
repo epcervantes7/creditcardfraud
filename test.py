7d49da2b75b391c33df84e79a6bb632aed909830
Evelyn Perez Cervantes <epcervantes7@gmail.com>
2022-03-29 22:01:11 -0300

    Update test

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
 
 print("hola mundo")
+
+import joblib
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (balanced_accuracy_score, classification_report,
+                             confusion_matrix, roc_auc_score)
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+plt.style.use('ggplot')
+#train
+df = pd.read_csv("data/creditcard.csv")
+df.shape
+#training data distribution
+df['Class'].value_counts().plot.bar()
